7d398a17559e7ac06bf3bcdf4d2a456df62a5530
AyoTFSG <adelekeayooluwa0617@gmail.com>
2025-06-03 13:29:09 +0100

    api commit

--- index.js
+++ index.js
@@ -1,63 +1,99 @@
-// const btn = document.querySelector('.login-button')
+// // const btn = document.querySelector('.login-button')
 
-// btn.addEventListener('click', function() {
-//   let email = document.getElementById('email').value;
-//   let password = document.getElementById('password').value;
+// // btn.addEventListener('click', function() {
+// //   let email = document.getElementById('email').value;
+// //   let password = document.getElementById('password').value;
 
-//   if (email === 'appclick@gmail.com' && password === '1234AppClick') {
-//     alert('Login successful!');
+// //   if (email === 'appclick@gmail.com' && password === '1234AppClick') {
+// //     alert('Login successful!');
 
-//     const welcome = document.getElementById('welcome-text');
-//     welcome.innerText = "Welcome Ayo!";
+// //     const welcome = document.getElementById('welcome-text');
+// //     welcome.innerText = "Welcome Ayo!";
 
-//     const container = document.getElementById('container');
-//     container.remove();
+// //     const container = document.getElementById('container');
+// //     container.remove();
     
 
-//   } else {
-//     alert('Incorrect email or password.');
+// //   } else {
+// //     alert('Incorrect email or password.');
+// //   }
+// // });
+
+// // btn.addEventListener('click', function() {
+// //   let email = document.getElementById('email').value;
+// //   let password = document.getElementById('password').value;
+
+// // if (email === "" && password === "") 
+// //     alert('Insert login details!');
+// // });
+
+// // fetch("https://dummyjson.com/products")
+// //   .then((response) => response.json())
+// //   .then((data) => console.log(data))
+// //   .catch((error) => console.log("error", error));
+
+// const getApi = async () => {
+//   try {
+//     const response = await fetch("https://dummyjson.com/products");
+//     const data = await response.json();
+//     displayProducts(data.products);
+//   } catch(error) {
+//     console.error("Error", error);
 //   }
-// });
+// };
 
-// btn.addEventListener('click', function() {
-//   let email = document.getElementById('email').value;
-//   let password = document.getElementById('password').value;
+// function displayProducts(products) {
+//   const list = document.getElementById("productList");
+//   list.innerHTML = "";
 
-// if (email === "" && password === "") 
-//     alert('Insert login details!');
-// });
+//   products.forEach((products) => {
+//     const li = document.createElement("li");
+//     li.className = "product";
+//     li.innerHTML = `
+//           <img src= "${products.thumbnail}" alt= "${products.title}">
+//           <h3>${products.title}</h3>
+//           <p>Price: $${products.price}</p>
+//           <p>${products.description.slice(0, 60)}...</p>
+//           `;
+//     list.appendChild(li);
+//   })
+// }
 
-// fetch("https://dummyjson.com/products")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log("error", error));
+
+//   window.addEventListener("DOMContentLoaded", getApi);
 
 const getApi = async () => {
   try {
     const response = await fetch("https://dummyjson.com/products");
     const data = await response.json();
-    displayProducts(data.products);
+    const product = data.products;
+    displayProducts(product);
   } catch(error) {
     console.error("Error", error);
   }
 };
 
 function displayProducts(products) {
-  const list = document.getElementById("productList");
+  const list = document.getElementById("catalogGrid");
   list.innerHTML = "";
 
-  products.forEach((products) => {
+  products.forEach((product) => {
     const li = document.createElement("li");
-    li.className = "product";
+    li.className = "item-card";
+
     li.innerHTML = `
-          <img src= "${products.thumbnail}" alt= "${products.title}">
-          <h3>${products.title}</h3>
-          <p>Price: $${products.price}</p>
-          <p>${products.description.slice(0, 60)}...</p>
-          `;
+      <span class="badge-stock">${product.availabilityStatus}</span>
+      <img src="${product.thumbnail}" alt="${product.title}">
+      <h3>${product.title}</h3>
+      <p><strong>Brand</strong>: ${product.brand}</p>
+      <p>Price: $${product.price}</p>
+      <p>${product.description.slice(0, 60)}...</p>
+      <p>review: ${product.reviews[0]?.comment || "No review available"}</p>
+      <p><strong>Shipping Information</strong>: ${product.shippingInformation}</p>
+    `;
+
     list.appendChild(li);
-  })
+  });
 }
 
-
-  window.addEventListener("DOMContentLoaded", getApi);
+window.addEventListener("DOMContentLoaded", getApi);
